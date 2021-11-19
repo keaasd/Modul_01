50c0b8721f678be1c8a2f1a3b2a642bfbc2aa549
Евгений Коротин <kea-asd@yandex.ru>
2021-11-19 22:06:17 +0700

    comitic

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+const productName = 'Рубашка';
+const quantity = 2;
+const productCategory = 'Одежда';
+const price = 1500;
+
+console.log(productName);
+console.log(price * quantity);
