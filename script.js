c58862012f7a7a58806c1521f9abe154b5a2986b
“Евгений <kea-asd@yandex.ru>
2021-11-22 22:29:43 +0700

    f

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 {
-const productName = 'Рубашка';
-const count = 2;
-const productCategory = 'Одежда';
-const price = 1500;
+const productName = 'Рубашка'; // наименование товара
+const count = 2; // количества товара
+const productCategory = 'Одежда'; // категория товара
+const price = 1500; // цена товара
 
-console.log(productName);
-console.log(`общая сумма товара: ${price * count}`);
+// console.log(productName);
+// console.log(`общая сумма товара: ${price * count}`);
 }
 
 {
@@ -15,6 +15,31 @@ const count = 4;
 const productCategory = 'Спорт-товары';
 const price = 799;
 
-console.log(productName);
-console.log(`общая сумма товара: ${price * count}`);
+// console.log(productName);
+// console.log(`общая сумма товара: ${price * count}`);
 }
+{
+const productName = 'Проездной';
+const count = 10;
+const productCategory = 'транспортные товары';
+const price = 375;
+
+// console.log(productName);
+// console.log(`общая сумма товара: ${price * count}`);
+}
+const productName = prompt('Напиши наименование товара');	
+
+const count = Number(prompt('количества товара, сколько надо-то? пиши цифрами')); 
+	
+const productCategory = prompt('а категория товара какая?');
+
+const price = Number(prompt('назови цену, договоримся')); 
+
+
+console.log(typeof productName);
+console.log(typeof count);
+console.log(typeof productCategory); 
+console.log(typeof price);
+
+console.log(`На складе ${count} единицы товара "${productName}" на сумму ${price * count}`);
+
