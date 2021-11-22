4871937b63f4e77a34dc61353e750828c17eb50e
keaasd <89311461+keaasd@users.noreply.github.com>
2021-11-22 14:30:13 +0700

    Add files via upload

---
+++ Modul_03/script.js
@@ -0,0 +1,20 @@
+'use strict';
+{
+const productName = 'Рубашка';
+const count = 2;
+const productCategory = 'Одежда';
+const price = 1500;
+
+console.log(productName);
+console.log(`общая сумма товара: ${price * count}`);
+}
+
+{
+const productName = 'Ласты';
+const count = 4;
+const productCategory = 'Спорт-товары';
+const price = 799;
+
+console.log(productName);
+console.log(`общая сумма товара: ${price * count}`);
+}
