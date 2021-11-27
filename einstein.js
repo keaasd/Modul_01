28e180b19685133116c5a9701c8baa4ac26d3348
“Евгений <kea-asd@yandex.ru>
2021-11-27 23:10:02 +0700

    rain

--- einstein.js
+++ einstein.js
@@ -1,7 +1,7 @@
 const myWeight = 67;
 const speedLight = 3e8;
 
-const e = myWeight * Math.pow(speedLight, 2);
+const e = myWeight * Math.pow(speedLight, 2); // возводим вдвое
 
 
 // console.log(e + " Дж");
--- tasks.js
+++ tasks.js
@@ -1,6 +1,4 @@
-const rain = (min, max) => {
-    return Math.random(0, 1);
-  }
+const rain = Math.random() > 0.5 ? 1 : 0;
 if (rain === 1) {
     console.log("Пошёл дождь. Возьмите зонт!");
 }
@@ -9,6 +7,20 @@ if (rain === 0) {
     console.log("Дождя нет!");
 }
 
+// const rain = (min, max) => {   const sum = Math.round(Math.random(min, max))    if (sum === 1) {     return console.log("Пошёл дождь. Возьмите зонт!");   }   return console.log("Дождя нет!"); }    rain(0, 1)
+// 3я задача
 
-// Math.Random и Math.round напишите выражение, которое будет возвращать 1 или 0
-// Math.Random(1, 0);
+const ball = 265;
+
+const matematika = Number(prompt('Введите кол-во баллов по математике:'));
+const rus = Number(prompt('Введите кол-во баллов по русскому:'));
+const informatika = Number(prompt('Введите кол-во баллов по информатике:'));
+
+let summa = +matematika + rus + informatika;
+console.log(matematika + rus + informatika);
+    
+if (summa >=  ball) {
+    console.log("Поздравляю, вы поступили на бюджет!")
+} else {
+    console.log("К сожалению, вы не поступили на бюджет!");
+}
