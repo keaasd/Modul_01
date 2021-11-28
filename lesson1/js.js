cd302deced26fe0b95b37be5bdab5e4523a10e1e
“Евгений <kea-asd@yandex.ru>
2021-11-29 00:28:09 +0700

    taskHard2

---
+++ lesson1/js.js
@@ -0,0 +1,18 @@
+
+
+// switch (client) {
+//     case "15000":
+//         nalog = client - (client * 0.13)
+//         console.log("15 тыщ - налог 13 процентов");
+//     break;
+    // case (50000):
+    //     nalog = client - (client * 0.2)
+    //     console.log("15 тыщ - налог 20 процентов");
+    // break;
+    // case (50000):
+    //     nalog = client - (client * 0.3)
+    //     console.log("15 тыщ - налог 30 процентов");
+    // break;
+    // default:
+    //     nalog = 0;    
+    //     break;
