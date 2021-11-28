8ab2b9d7d5c54110923620e8a171d093590e7279
“Евгений <kea-asd@yandex.ru>
2021-11-29 00:30:36 +0700

    hard

--- lesson4/tasksHard_1.js
+++ lesson4/tasksHard_1.js
@@ -1,17 +1,17 @@
 const client = Number(prompt('Какой ваш доход?')); 
 let nalog = 0;
 
-if (client < 15000) {
+if (client <= 15000) {
     nalog = client * 0.2;
     console.log("налог 13 % от суммы " + client)
 };
 
-if (client >= 15000 && client < 50000) {
+if (client > 15000 && client <= 50000) {
     nalog = client * 0.2;
     console.log("налог 20 % от суммы " + client)
 };
 
-if (client >= 50000) {
+if (client > 50000) {
     nalog = client * 0.3;
     console.log("налог 30 % от суммы " + client)
 };
